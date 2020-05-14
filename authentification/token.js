d4dac5433daeeb5e4f131794d1d156b868a75e2f
Destiny Erhabor <destinterhabor6@gmail.com>
2020-05-14 02:23:36 +0100

    auth rights and privilledges

---
+++ authentification/token.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+
+
+const token = async (req, res, next) => {
+    const token = req.header('Authorization');
+    if(!token) return res.status(401).json("no token provided");
+    try {
+        req.user = jwt.verify(token, 'caesarsage');
+        next();
+    } catch (err) {
+        res.status(400).json("Invalid token!")
+    }
+}
+
+module.exports = {
+    token
+}
