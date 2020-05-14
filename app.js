c384a5dba08516338734876bd23481d64ecf24bf
Destiny Erhabor <destinterhabor6@gmail.com>
2020-05-14 03:09:23 +0100

    database connected

--- app.js
+++ app.js
@@ -36,10 +36,13 @@ app.use('/v1', User, (req,res)=> {
 //create a port
 const port = process.env.PORT || 3000;
 //connect to a database
-mongoose.connect('mongodb://localhost/', {useNewUrlParser: true,
+//mongoose.connect('mongodb://localhost/')
+process.env.databaseURL
+mongoose.connect('mongodb://caesar:mighty@cluster0-shard-00-00-r7d4u.mongodb.net:27017,cluster0-shard-00-01-r7d4u.mongodb.net:27017,cluster0-shard-00-02-r7d4u.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {useNewUrlParser: true,
 useUnifiedTopology:true}
 )
 .then(result => {
+    console.log('database connected')
     console.log(`Running on port ${port}`);
 app.listen(port)
 });
