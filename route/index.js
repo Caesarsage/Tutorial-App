f8c80f1fd6d36fbe778edbd9aeddb08470dccfec
Destiny Erhabor <destinterhabor6@gmail.com>
2020-05-16 04:08:55 +0100

    home edit

--- route/index.js
+++ route/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 
 router.get('/', (req, res)=>{
-    return res.send('Welcome To The Online Tutorial App!!!');
+    return res.status(200).json('Welcome To The Online Tutorial App!!!');
 });
 
 module.exports = router;
